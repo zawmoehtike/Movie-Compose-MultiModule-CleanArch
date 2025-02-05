d712a5768d8c48974f8deb0f7aace2b9dad9bad7
Zaw Moe Htike <zaw.h@unii.co.th>
2025-02-06 00:20:45 +0700

    import libraries in gradles in modules (part 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -50,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -58,4 +61,25 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.activity.activity)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
+
+    implementation(libs.androidx.compose.animation.core)
+    implementation(libs.androidx.compose.animation)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // third parties
+    implementation(libs.compose.viewmodel)
+    implementation(libs.timber)
+    implementation(libs.coil)
+    implementation(libs.bundles.paging)
+    implementation(libs.constraint.compose)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
